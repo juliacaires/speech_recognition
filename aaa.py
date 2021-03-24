ed879a6592b6c6464da60d64d706be364a19e579
juliacaires <81318018+juliacaires@users.noreply.github.com>
2021-03-24 14:16:14 -0300

    Add files via upload

---
+++ aaa.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+r= sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('Speak Anything: ')
+    audio = r.listen(source)
+
+    text= r.recognize_google(audio)
+    print('You said : {}'.format(text))
+
+
+
+
+
+
